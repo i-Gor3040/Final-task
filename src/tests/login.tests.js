394953ea9290b913f1951152857ad32aa1e0e0cc
i-Gor <i.gorshchuk@gmail.com>
2025-11-16 18:21:58 +0200

    Updated UC-1

--- src/tests/login.tests.js
+++ src/tests/login.tests.js
@@ -1,10 +1,31 @@
+import { faker } from "@faker-js/faker";
+
 describe("Login tests", () => {
   beforeEach(async () => {
     await browser.url("/");
   });
 
-  it("UC-1: Login form with empty credentials", async () => {
-    await expect(browser).toHaveTitle("Swag Labs");
+  it.only("UC-1: Login form with empty credentials", async () => {
+    // await expect(browser).toHaveTitle("Swag Labs");
+    const username = faker.internet.username({
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+    });
+
+    const password = faker.internet.password();
+    // console.log(username);
+    // console.log(password);
+
+    await $('[data-test="username"]').setValue(username);
+    await $('[data-test="password"]').setValue(password);
+    await $('[data-test="username"]').setValue(" ");
+    await $('[data-test="password"]').setValue("");
+    await browser.pause(1000);
+
+    await $('[data-test="login-button"]').click();
+    await expect($('[data-test="error"]')).toHaveText(
+      "Epic sadface: Username is required"
+    );
   });
 });
 /**
